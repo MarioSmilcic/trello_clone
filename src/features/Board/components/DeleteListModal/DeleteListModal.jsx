76c389cdd23145dcd7cac5f02f2ed2472f0e3bc0
Mario <mariosmilcic@gmail.com>
2024-07-09 12:01:01 +0200

    removed AddCardModal and AddListModal components

--- src/features/Board/components/DeleteListModal/DeleteListModal.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import CardModal from "../CardModal/CardModal";
-
-const DeleteListModal = () => {
-  return <div>{/* <CardModal></CardModal> */}</div>;
-};
-
-export default DeleteListModal;
