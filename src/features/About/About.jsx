55a2bab437e1195b81bd7fe18a0469c16effe0ca
Mario <mariosmilcic@gmail.com>
2024-07-13 11:38:08 +0200

    added about page

--- src/features/About/About.jsx
+++ src/features/About/About.jsx
@@ -4,21 +4,40 @@ const About = () => {
   return (
     <div className="about">
       <h2>About</h2>
+
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga adipisci
-        perferendis ex facere sapiente nostrum sint similique. Repudiandae,
-        dolor reiciendis. Cumque sint, repellendus accusamus asperiores
-        praesentium officiis ipsam! Iure quibusdam quae possimus quasi!
-        Veritatis, minima hic quibusdam officiis non iusto pariatur quaerat
-        omnis aperiam minus ullam quos enim dignissimos inventore?
+        Welcome to our task management app! This application is designed to help
+        you organize and manage your tasks and projects efficiently.
       </p>
+
+      <h2>Technologies Used:</h2>
+      <div className="about-list">
+        <ul>
+          <li>
+            <span className="about-span">React:</span> For building the user
+            interface.
+          </li>
+          <li>
+            <span className="about-span">React Router:</span>
+            For handling navigation and routing within the app.
+          </li>
+          <li>
+            <span className="about-span">Zustand:</span> For state management.
+          </li>
+          <li>
+            <span className="about-span">@dnd-kit:</span> For implementing the
+            drag-and-drop functionality.
+          </li>
+          <li>
+            <span className="about-span">Vanilla CSS:</span> For styling the
+            components.
+          </li>
+        </ul>
+      </div>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga adipisci
-        perferendis ex facere sapiente nostrum sint similique. Repudiandae,
-        dolor reiciendis. Cumque sint, repellendus accusamus asperiores
-        praesentium officiis ipsam! Iure quibusdam quae possimus quasi!
-        Veritatis, minima hic quibusdam officiis non iusto pariatur quaerat
-        omnis aperiam minus ullam quos enim dignissimos inventore?
+        We hope this app helps you stay organized and enhances your
+        productivity. Thank you for using our task management tool! If you have
+        any questions or feedback, please feel free to contact us.
       </p>
     </div>
   );
