27a50f5826e0d93d6481f966852a12865750910e
Mario <mariosmilcic@gmail.com>
2024-10-02 10:00:31 +0200

    refactored and reduced logic for drag and drop operations

--- src/features/Board/Card.jsx
+++ src/features/Board/Card.jsx
@@ -1,18 +1,19 @@
 import "./styles/card.style.css";
 import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 import EditIcon from "../../components/icons/EditIcon";
 import TrashIcon from "../../components/icons/TrashIcon";
 import EditCardModal from "./components/EditCardModal/EditCardModal";
 import DeleteModal from "./components/DeleteModal/DeleteModal";
 import { useModalsStore } from "../../store/modals/modals.store";
 
-const Card = ({ card, listId, cardIndex }) => {
+const Card = ({ card, listId, index }) => {
   const { modal, openModal } = useModalsStore();
 
   const {
-    setNodeRef,
     attributes,
     listeners,
+    setNodeRef,
     transform,
     transition,
     isDragging,
@@ -20,29 +21,19 @@ const Card = ({ card, listId, cardIndex }) => {
     id: card.id,
     data: {
       type: "Card",
-      cardId: card.id,
-      listId,
-      cardIndex,
+      card,
+      list: { id: listId },
+      index,
     },
   });
 
   const style = {
+    transform: CSS.Transform.toString(transform),
     transition,
     opacity: isDragging ? 0.5 : 1,
-    transform: isDragging
-      ? `translate3d(${transform?.x}px, ${transform?.y}px, 0)`
-      : "none",
   };
 
   const isCurrentCard = modal?.props?.cardId === card.id;
-
-  if (isDragging) {
-    return (
-      <div ref={setNodeRef} style={style} className="card-dragging">
-        {card.card}
-      </div>
-    );
-  }
 
   const handleEditClick = () =>
     openModal("editCard", { cardId: card.id, listId });
