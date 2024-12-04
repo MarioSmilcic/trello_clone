cc69f6a114d06e5563d76c7598e4cb61ba69b8c8
Mario <mariosmilcic@gmail.com>
2024-12-04 22:54:00 +0100

    added BoardInput and BoardTextArea components, reduced styling logic, refactored and restyled listActions

--- src/features/Board/modals/EditListModal.jsx
+++ src/features/Board/modals/EditListModal.jsx
@@ -1,44 +1,33 @@
-import Button from "@/components/Button/Button";
 import { useState } from "react";
+import { BoardInput } from "../ui";
+import Button from "@components/Button/Button";
 import { Close } from "@components/icons";
-import { useModalsStore } from "@/store/modals/modals.store";
-import { useLists, useKeyboard } from "../hooks";
+import { useModalsStore } from "@store/modals/modals.store";
+import { useLists } from "../hooks";
 
 const EditListModal = ({ listId, listTitle }) => {
   const [enteredTitle, setEnteredTitle] = useState(listTitle);
   const { closeModal } = useModalsStore();
   const { handleUpdateList } = useLists();
-  const { handleKeyPress } = useKeyboard();
 
-  const handleEnteredTitle = (e) => {
-    setEnteredTitle(e.target.value);
-  };
-
-  const handleUpdateListTitle = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     handleUpdateList(enteredTitle, listId);
   };
 
-  const onKeypressHandler = (e) => {
-    handleKeyPress(e, handleUpdateListTitle);
-  };
-
   return (
-    <form onSubmit={handleUpdateListTitle} className="editList-form">
-      <div className="editList">
-        <textarea
-          name="edit-modal"
-          id={listId}
-          className="editModal-input"
+    <form onSubmit={handleSubmit} className="edit-form">
+      <div className="edit-modal">
+        <BoardInput
           value={enteredTitle}
-          onChange={handleEnteredTitle}
-          autoFocus
-          onKeyDown={onKeypressHandler}
+          onChange={(e) => setEnteredTitle(e.target.value)}
+          onSubmit={handleSubmit}
+          placeholder="Edit list title..."
+          name="editListTitle"
         />
-
-        <div className="add-listModal_buttons">
+        <div className="button-container">
           <Button text="Save" />
-          <div className="edit-listModal_close">
+          <div className="close-button">
             <Close onClose={closeModal} />
           </div>
         </div>
